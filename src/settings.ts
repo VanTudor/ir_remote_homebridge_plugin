b506ce117afe80190a0cdee277178d7102ba55f2
vantudor <tudor.leustean@gmail.com>
2020-08-30 12:31:54 +0300

    changed some names and whatnot

--- src/settings.ts
+++ src/settings.ts
@@ -2,9 +2,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'IRREmoteEmulator';
+export const PLATFORM_NAME = 'IRRemoteEmulator';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'ir-remote-emulator';
+export const PLUGIN_NAME = 'homebridge-ir-remote-emulator';
