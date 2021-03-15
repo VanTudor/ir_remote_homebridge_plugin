33532cb5824d446bbb20a1da9c2fc74cd19abc4f
vantudor <tudor.leustean@gmail.com>
2021-03-15 16:06:26 +0200

    minor fixes

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -2,6 +2,7 @@ import got from "got";
 import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
 
 import { IRRemoteEmulatorPlatform } from './platform';
+import { TBrightnessCommands, TColorCommands, TOnOffCommands } from "./types";
 
 export enum ECommandTypes {
   BRIGHTNESS_UP = 'BRIGHTNESS_UP',
@@ -24,24 +25,6 @@ export enum ECommandTypes {
   RED = 'RED',
   WHITE = 'WHITE'
 }
-
-export type TBrightnessCommands = ECommandTypes.BRIGHTNESS_UP | ECommandTypes.BRIGHTNESS_DOWN;
-export type TColorCommands = ECommandTypes.DARK_ORANGE |
-  ECommandTypes.ORANGE |
-  ECommandTypes.LIGHT_ORANGE |
-  ECommandTypes.YELLOW |
-  ECommandTypes.LIGHT_GREEN |
-  ECommandTypes.GREEN |
-  ECommandTypes.TEAL |
-  ECommandTypes.LIGHT_BLUE |
-  ECommandTypes.BLUE |
-  ECommandTypes.DARK_BLUE |
-  ECommandTypes.PURPLE |
-  ECommandTypes.PINK |
-  ECommandTypes.DARK_PINK |
-  ECommandTypes.RED |
-  ECommandTypes.WHITE;
-export type TOnOffCommands = ECommandTypes.ON | ECommandTypes.OFF;
 
 function getBrightnessInstructions(prevValue: number, currentValue: number): { steps: number, command: TBrightnessCommands } {
   const diff = Math.floor(prevValue / 20) - Math.floor(currentValue / 20);
@@ -68,7 +51,6 @@ export class ExamplePlatformAccessory {
     Brightness: 100,
     Hue: 0,
     Saturation: 50,
-    ColorTemperature: 10,
     DeviceColor: 'WHITE'
   }
 
@@ -83,41 +65,24 @@ export class ExamplePlatformAccessory {
       .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
       .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
     this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.On)
-        .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-        .on('get', this.getOn.bind(this))                // GET - bind to the `getOn` method below
+        .on('set', this.setOn.bind(this))
+        .on('get', this.getOn.bind(this))
 
-    // register handlers for the Brightness Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-        .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
+        .on('set', this.setBrightness.bind(this));
     this.service.getCharacteristic(this.platform.Characteristic.Hue)
         .on('set', this.setHue.bind(this))
         .on('get', this.getHue.bind(this));
     this.service.getCharacteristic(this.platform.Characteristic.Saturation)
         .on('set', this.setSaturation.bind(this))
         .on('get', this.getSaturation.bind(this));
-    // this.service.getCharacteristic(this.platform.Characteristic.ColorTemperature)
-    //     .on('set', this.setColorTemperature.bind(this))
-    //     .on('get', this.getColorTemperature.bind(this));
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
   async setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
     const commandName: TOnOffCommands = !!value ? ECommandTypes.ON : ECommandTypes.OFF
@@ -259,7 +224,7 @@ export class ExamplePlatformAccessory {
         deviceColor = ECommandTypes.WHITE;
     }
     // override color if saturation low
-    if (this.state.Saturation < 90) {
+    if (this.state.Saturation < 75) {
       deviceColor = ECommandTypes.WHITE;
     }
     if (this.state.DeviceColor !== deviceColor) {
@@ -274,7 +239,6 @@ export class ExamplePlatformAccessory {
     this.platform.log.debug('Set Characteristic HUE -> ', value);
     this.platform.log.debug('Set Characteristic HUE. Selected DB color -> ', deviceColor);
 
-    // you must call the callback function
     callback(null);
   }
 
@@ -323,29 +287,4 @@ export class ExamplePlatformAccessory {
     callback(null, saturation);
   }
 
-
-  // async setColorTemperature(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-  //   this.platform.log.debug('ColorTemperature VALUE:', value);
-  //
-  //   // implement your own code to set the brightness
-  //   this.state.ColorTemperature = value as number;
-  //
-  //   this.platform.log.debug('Set Characteristic ColorTemperature -> ', value);
-  //
-  //   // you must call the callback function
-  //   callback(null);
-  // }
-  //
-  // getColorTemperature(callback: CharacteristicGetCallback) {
-  //
-  //   // implement your own code to check if the device is on
-  //   const colorTemperature = this.state.ColorTemperature;
-  //
-  //   this.platform.log.debug('Get Characteristic ColorTemperature ->', colorTemperature);
-  //
-  //   // you must call the callback function
-  //   // the first argument should be null if there were no errors
-  //   // the second argument should be the value to return
-  //   callback(null, colorTemperature);
-  // }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { ECommandTypes } from "./platformAccessory";
+
 export interface IPaginatedResponse<T> {
   rows: T[],
   total: number
@@ -11,3 +13,21 @@ export interface IDevice {
     "createdAt": Date,
     "updatedAt": Date
 }
+
+export type TBrightnessCommands = ECommandTypes.BRIGHTNESS_UP | ECommandTypes.BRIGHTNESS_DOWN;
+export type TColorCommands = ECommandTypes.DARK_ORANGE |
+  ECommandTypes.ORANGE |
+  ECommandTypes.LIGHT_ORANGE |
+  ECommandTypes.YELLOW |
+  ECommandTypes.LIGHT_GREEN |
+  ECommandTypes.GREEN |
+  ECommandTypes.TEAL |
+  ECommandTypes.LIGHT_BLUE |
+  ECommandTypes.BLUE |
+  ECommandTypes.DARK_BLUE |
+  ECommandTypes.PURPLE |
+  ECommandTypes.PINK |
+  ECommandTypes.DARK_PINK |
+  ECommandTypes.RED |
+  ECommandTypes.WHITE;
+export type TOnOffCommands = ECommandTypes.ON | ECommandTypes.OFF;
